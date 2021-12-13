8b1b4b82014de82383d4875359a7995bc4d3bffe
Hasan Can Bayrak <bayrakhasancan@gmail.com>
2021-12-13 22:27:21 +0300

    WebAPI added. Autofac , FluentValidation added.

---
+++ Business/ValidationRules/FluentValidation/WordValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class WordValidator : AbstractValidator<Word>
+    {
+        public WordValidator()
+        {
+            RuleFor(word => word.Text).NotEmpty();
+            RuleFor(word => word.TranslatedText).NotEmpty();
+
+        }
+    }
+}
---
+++ WebAPI/Controllers/WordsController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        IWordService _wordService;
+
+        public WordsController(IWordService wordService)
+        {
+            _wordService = wordService;
+        }
+
+        [HttpGet]
+        public IDataResult<List<Word>> GetWords()
+        {
+            return _wordService.GetAll();
+        }
+    }
+}
